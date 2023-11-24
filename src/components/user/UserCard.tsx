c42ea0decf258df16538c4a32f10f2a5c51a8c17
nanoha <nanoha.creator@gmail.com>
2023-11-24 16:21:29 +0000

    ユーザー一覧実装

---
+++ src/components/user/UserCard.tsx
@@ -0,0 +1,40 @@
+import React, { memo } from "react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+
+interface Props {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+}
+
+const UserCard = memo(({ imageUrl, userName, fullName }: Props) => {
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full"
+          boxSize="160px"
+          src={imageUrl}
+          alt="プロフィール画像"
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
+
+export default UserCard;
--- src/pages/UserManagement.tsx
+++ src/pages/UserManagement.tsx
@@ -1,7 +1,36 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import UserCard from "../components/user/UserCard";
+import useAllUsers from "../hooks/useAllUsers";
 
 const UserManagement = memo(() => {
-  return <div>UserManagement</div>;
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl={"https://source.unsplash.com/random"}
+                userName={user.username}
+                fullName={user.name}
+              />{" "}
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
 
 export default UserManagement;
